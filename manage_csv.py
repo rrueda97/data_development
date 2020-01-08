1fa630137e68c9c82b339e3c55c46e4581a2e219
Ricky Rueda <ricardo.rda97@gmail.com>
2020-01-07 21:27:07 -0800

    script to handle a lot of different csv stuff

--- manage_csv.py
+++ manage_csv.py
@@ -108,7 +108,7 @@ def filter_bad():
     filtered_rows = []
     with open(csv_path, mode='r') as f:
         for row in csv.reader(f):
-            if (row[17] == 'TRUE') or (row[18] == 'TRUE'):
+            if (row[17] == 'True') or (row[18] == 'True'):
                 fname_vid = row[0]
                 fname_joints = fname_vid[:-len('.avi')]+'_joints.tensor'
                 print(fname_vid)
@@ -119,7 +119,6 @@ def filter_bad():
     with open(csv_path[:-len('.csv')]+'_filtered.csv', mode='w') as filtered:
         writer = csv.writer(filtered)
         writer.writerows(filtered_rows)
-
 
 
 def merge_csv():
@@ -144,6 +143,102 @@ def merge_csv():
         writer.writerows(merged_rows)
 
 
+def get_count():
+    inp = input('Path to csv file: CurrentDir/')
+    csv_path = os.path.join(os.getcwd(),inp)
+    falling_RRueda = 0
+    falling_ATam = 0
+    falling_ATwombly = 0
+    sitting_RRueda = 0
+    sitting_ATam = 0
+    sitting_ATwombly = 0
+    standing_RRueda = 0
+    standing_ATam = 0
+    standing_ATwombly = 0
+    rollingBed_RRueda = 0
+    rollingBed_ATam = 0
+    rollingBed_ATwombly = 0
+    stillBed_RRueda = 0
+    stillBed_ATam = 0
+    stillBed_ATwombly = 0
+    rollingGround_RRueda = 0
+    rollingGround_ATam = 0
+    rollingGround_ATwombly = 0
+    stillGround_RRueda = 0
+    stillGround_ATam = 0
+    stillGround_ATwombly = 0
+    with open(csv_path, mode='r') as f:
+        for row in csv.reader(f):
+            action = row[2]
+            person = row[3]
+            bad = row[17]
+            quest = row[19]
+            if (bad == 'False' or bad =='FALSE') and (quest == 'False' or quest =='FALSE'):
+                if action == 'Falling':
+                    if person == 'VBaena':
+                        falling_RRueda += 1
+                    elif person == 'ATam':
+                        falling_ATam += 1
+                    elif person == 'ATwombly':
+                        falling_ATwombly += 1
+                elif action == 'SittingUp':
+                    if person == 'VBaena':
+                        sitting_RRueda += 1
+                    elif person == 'ATam':
+                        sitting_ATam += 1
+                    elif person == 'ATwombly':
+                        sitting_ATwombly += 1
+                elif action == 'Standing':
+                    if person == 'VBaena':
+                        standing_RRueda += 1
+                    elif person == 'ATam':
+                        standing_ATam += 1
+                    elif person == 'ATwombly':
+                        standing_ATwombly += 1
+                elif action == 'RollingBed':
+                    if person == 'VBaena':
+                        rollingBed_RRueda += 1
+                    elif person == 'ATam':
+                        rollingBed_ATam += 1
+                    elif person == 'ATwombly':
+                        rollingBed_ATwombly += 1
+                elif action == 'StillBed':
+                    if person == 'VBaena':
+                        stillBed_RRueda += 1
+                    elif person == 'ATam':
+                        stillBed_ATam += 1
+                    elif person == 'ATwombly':
+                        stillBed_ATwombly += 1
+                elif action == 'RollingGround':
+                    if person == 'VBaena':
+                        rollingGround_RRueda += 1
+                    elif person == 'ATam':
+                        rollingGround_ATam += 1
+                    elif person == 'ATwombly':
+                        rollingGround_ATwombly += 1
+                elif action == 'StillGround':
+                    if person == 'VBaena':
+                        stillGround_RRueda += 1
+                    elif person == 'ATam':
+                        stillGround_ATam += 1
+                    elif person == 'ATwombly':
+                        stillGround_ATwombly += 1
+    print('Falling:\n    VBaena: '+str(falling_RRueda)+'\n    ATam: '+str(
+        falling_ATam)+'\n    ATwombly: '+str(falling_ATwombly))
+    print('Sitting Up:\n    VBaena: ' + str(sitting_RRueda) + '\n    ATam: ' + str(
+        sitting_ATam) + '\n    ATwombly: ' + str(sitting_ATwombly))
+    print('Standing:\n    VBaena: ' + str(standing_RRueda) + '\n    ATam: ' + str(
+        standing_ATam) + '\n    ATwombly: ' + str(standing_ATwombly))
+    print('Rolling Bed:\n    VBaena: ' + str(rollingBed_RRueda) + '\n    ATam: ' + str(
+        rollingBed_ATam) + '\n    ATwombly: ' + str(rollingBed_ATwombly))
+    print('Still Bed:\n    VBaena: ' + str(stillBed_RRueda) + '\n    ATam: ' + str(
+        stillBed_ATam) + '\n    ATwombly: ' + str(stillBed_ATwombly))
+    print('Rolling Ground:\n    VBaena: ' + str(rollingGround_RRueda) + '\n    ATam: ' + str(
+        rollingGround_ATam) + '\n    ATwombly: ' + str(rollingGround_ATwombly))
+    print('Still Ground:\n    VBaena: ' + str(stillGround_RRueda) + '\n    ATam: ' + str(
+        stillGround_ATam) + '\n    ATwombly: ' + str(stillGround_ATwombly))
+
+
 
 if __name__ == '__main__':
     # csv_dir = input('Path to csv file:')
@@ -155,4 +250,5 @@ if __name__ == '__main__':
     # compare_vid_csv(csv_path,data_path)
     # delete_empty_rows(csv_path)
     # merge_csv()
-    filter_bad()
+    # filter_bad()
+    get_count()
