cf3f52015184177ad60bb0204ece89444d939af0
Ricky Rueda <ricardo.rda97@gmail.com>
2021-01-27 09:08:04 -0800

    paginated query for labeling database

--- pull_aws_data.py
+++ pull_aws_data.py
@@ -119,16 +119,98 @@ def show_img(img_path, detection_path):
     k = cv2.waitKey(0)
     return k
 
+def get_data_dictionary(data_path):
+    """returns a dictionary with different class names as keys and lists of image and box file names"""
+    # todo: sort original images by datetime, excess data = newest data
+    data_dict = {}
+    for dir in os.listdir(data_path):
+        dir_path = os.path.join(data_path, dir)
+        if not os.path.isdir(dir_path):
+            continue
+        imgs = [f for f in os.listdir(dir_path) if f.endswith('.png')]
+        boxes = [img[:-len('.png')] + '.txt' for img in imgs]
+        data_dict[dir] = {'imgs': imgs, 'boxes': boxes}
+    return data_dict
+
+def split_data(percent_holdout: float, train_set_path: str):
+    """creates a validation set from an existing training set of data"""
+
+    data_dict = get_data_dictionary(train_set_path)
+    min_class_size = min([len(data_dict[cls]['imgs']) for cls in data_dict])
+    print(f"Trimming Data to N = {min_class_size/2}...")
+    # trim excess data
+    excess_data_dict = {cls: {'imgs': data_dict[cls]['imgs'][min_class_size:],
+                              'boxes': data_dict[cls]['boxes'][min_class_size:]}
+                        for cls in data_dict}
+    excess_data_path = os.path.join(os.path.dirname(train_set_path), 'excess_data')
+    if not os.path.exists(excess_data_path):
+        os.mkdir(excess_data_path)
+    for cls in excess_data_dict:
+        class_dir = os.path.join(excess_data_path, cls)
+        if not os.path.exists(class_dir):
+            os.mkdir(class_dir)
+        for i in range(len(excess_data_dict[cls]['imgs'])):
+            src_path_img = os.path.join(train_set_path, cls, excess_data_dict[cls]['imgs'][i])
+            os.rename(src_path_img, os.path.join(class_dir, excess_data_dict[cls]['imgs'][i]))
+            src_path_box = os.path.join(train_set_path, cls, excess_data_dict[cls]['boxes'][i])
+            os.rename(src_path_box, os.path.join(class_dir, excess_data_dict[cls]['boxes'][i]))
+
+    holdout_idx = int(min_class_size * percent_holdout)
+    print(f'Splitting Data into N_val = {holdout_idx/2} N_train = {(min_class_size - holdout_idx)/2}...')
+    trimmed_data_dict = get_data_dictionary(train_set_path)
+    val_data_dict = {cls: {'imgs': trimmed_data_dict[cls]['imgs'][:holdout_idx],
+                           'boxes': trimmed_data_dict[cls]['boxes'][:holdout_idx]}
+                     for cls in trimmed_data_dict}
+    val_data_path = os.path.join(os.path.dirname(train_set_path), 'val_set')
+    if not os.path.exists(val_data_path):
+        os.mkdir(val_data_path)
+    for cls in val_data_dict:
+        class_dir = os.path.join(val_data_path, cls)
+        if not os.path.exists(class_dir):
+            os.mkdir(class_dir)
+        for i in range(len(val_data_dict[cls]['imgs'])):
+            src_path_img = os.path.join(train_set_path, cls, val_data_dict[cls]['imgs'][i])
+            os.rename(src_path_img, os.path.join(class_dir, val_data_dict[cls]['imgs'][i]))
+            src_path_box = os.path.join(train_set_path, cls, val_data_dict[cls]['boxes'][i])
+            os.rename(src_path_box, os.path.join(class_dir, val_data_dict[cls]['boxes'][i]))
+
+def check_sorted_data(data_path):
+    data_dict = get_data_dictionary(data_path)
+    for cls in data_dict:
+        imgs = data_dict[cls]['imgs']
+        boxes = data_dict[cls]['boxes']
+        for i in range(len(imgs)):
+            img_path = os.path.join(data_path, cls, imgs[i])
+            try:
+                assert os.path.exists(img_path)
+            except AssertionError:
+                print(img_path)
+            box_path = os.path.join(data_path,cls, boxes[i])
+            try:
+                assert os.path.exists(box_path)
+            except AssertionError:
+                print(box_path)
+
+
 
 if __name__ == '__main__':
-    synced_data_path = '/Users/ricardorueda/Data/livedevicepersoncapture_client1'
-    sorted_data_path = '/Users/ricardorueda/Data/livedevicepersoncapture_client1_09-2020_sorted'
-    # main(data_path=synced_data_path,
-    #      sorted_data_path=sorted_data_path)
-    # for dir in os.listdir(sorted_data_path):
-    #     dir_path = os.path.join(sorted_data_path, dir)
-    #     if not os.path.isdir(dir_path):
-    #         continue
-    #     sort_imgs(dir_path)
+    synced_data_path = '/Users/ricardorueda/Data/client1_november-2020'
+    sorted_data_path = '/Users/ricardorueda/Data/client1_november-2020_sorted'
+    main(synced_data_path, sorted_data_path)
+    # imgs = [f for f in os.listdir(sorted_data_path) if f.endswith('.png')]
+    # boxes = [img[:-len('.png')]+'.txt' for img in imgs]
+    # for i in range(len(imgs)):
+    #     img_path = os.path.join(sorted_data_path, imgs[i])
+    #     box_path = os.path.join(sorted_data_path, boxes[i])
+    #     k = show_img(img_path, box_path)
+    #     if k == 27:
+    #         os.rename(img_path, os.path.join(os.path.dirname(sorted_data_path), imgs[i]))
+    #         os.rename(box_path, os.path.join(os.path.dirname(sorted_data_path), boxes[i]))
+    # training_data_path = '/Users/ricardorueda/Data/NeuralNetData_client1_08-10_2020/train_set'
+    # check_sorted_data(training_data_path)
+    # split_data(percent_holdout=0.2, train_set_path=training_data_path)
+
+
+
 
 
